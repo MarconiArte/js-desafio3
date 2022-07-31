b57012f475a743fcb74bd0a5c03891ae56f92e12
Marcos Niz <marconi4305@gmail.com>
2022-07-31 18:52:47 -0300

    Se agrega proyecto

---
+++ app.js
@@ -0,0 +1,21 @@
+const ilustracionUno = {
+    nombre: "Chico de pelo violeta",
+    precio: 130,
+    creacion: "10/05/2020"
+}
+
+const ilustracionDos = {
+    nombre: "Chica pelirroja",
+    precio: 140,
+    creacion: "23/05/2020"
+}
+
+const ilustracionTres = {
+    nombre: "Chica realista",
+    precio: 230,
+    creacion: "30/07/2020"
+}
+
+const ilustraciones = [ilustracionUno, ilustracionDos, ilustracionTres]
+
+console.log(ilustraciones)
